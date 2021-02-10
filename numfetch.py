fd0f22590bca8658bb50f57982ed54f93c90bbf1
The-ZmaZe <74611799+The-ZmaZe@users.noreply.github.com>
2021-02-10 17:11:49 +0100

    Update numfetch.py

--- numfetch.py
+++ numfetch.py
@@ -24,8 +24,8 @@ sys_info = (
   "Shell: " + "µpython " + uname["version"],
   "Resolution: 320x240",
   "MCU: " + ("STM32F730V8T6" if uname["machine"] == "NumWorks N110" else "STM32F412"),
-  "Memory: " + ("8Mo" if uname["machine"] == "NumWorks N110" else "1Mo")
-  "RAM: " + ("256Ko" if uname["machine"] == "NumWorks N110" else "1Mo")
+  "Memory: " + ("8Mo" if uname["machine"] == "NumWorks N110" else "1Mo"),
+  "RAM: " + ("256Ko" if uname["machine"] == "NumWorks N110" else "1Mo"),
   "Screen: " + ("IPS-LCD" if uname["machine"] == "NumWorks N110" else "TFT-LCD")
 )
 
