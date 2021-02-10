d2edfdf79b7da12a8a0efe8a3ad0b684d528108e
Tim-ats-d <tim.arnouts@protonmail.com>
2021-02-10 11:05:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nfetch
+
+Tiny system info for [Omega](https://github.com/Omega-Numworks/Omega) operating systems.
+
+Does not work with [Epsilon](https://github.com/numworks/epsilon) (`os` µpython module is not available).
---
+++ nfetch.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+
+uname = os.uname()
+
+ASCII_LOGO = """                 
+  %%%%%%%%%%%    
+ %%%        %%%  
+%%%          %%  
+ %%%        %%   
+   %%%   %%%     
+%%%%%%   %%%%%%  
+                 
+                 
+"""
+
+sys_info = (
+  uname["sysname"],
+  len(uname["sysname"])*"-",
+  "OS: " + uname["release"],
+  "Host: " + uname["machine"],
+  "Uptime: " + str(time.monotonic()),
+  "Shell: " + "µpython " + uname["version"],
+  "Resolution: 320x240",
+  "MCU: " + ("STM32F730V8T6" if uname["machine"] == "NumWorks N110" else "STM32F412"),
+  "Memory: " + ("8Mo" if uname["machine"] == "NumWorks N110" else "1Mo")
+)
+
+for line, info in zip(ASCII_LOGO.split("\n"), sys_info):
+  print(line + info)
+ 
